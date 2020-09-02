a7a63ecf4eb94928ca1e9acd0bd643ee462aa301
Jakub Kułaga <kuba.kulaga.sv7@gmail.com>
2020-09-02 17:13:52 +0000

    Fix nav. Remove bootstrap from package.json.Add linkbutton shortcode

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -1,12 +1,17 @@
 <?php
 
-function linkBtn($atts)
+function linkBtn_func($atts)
 {
     $link = $atts['link'] ?? null;
     if (!$link || empty($link)) {
         return '';
     }
 
-    return Timber::render('shortcodes/linkButton.twig', ['atts' => $atts]);
+    $text = $atts['text'] ?? 'Zapisz się';
+    return <<<EOF
+    <div>
+        <a class="block float-right bg-orange-400 hover:bg-orange-500 p-1 rounded font-bold text-white p-4 rounded hover:text-white" href="$link">$text</a>
+    </div>
+EOF;
 }
-add_shortcode('linkBtn', 'linkBtn');
+add_shortcode('linkBtn', 'linkBtn_func');
