83de382420c744565ba309e39491424d7d7cedd8
Jakub Kułaga <kuba.kulaga.sv7@gmail.com>
2020-09-17 12:17:08 +0000

    Remove unused twig function.

--- functions.php
+++ functions.php
@@ -2,109 +2,96 @@
 
 // If the Timber plugin isn't activated, print a notice in the admin.
 if (!class_exists('Timber')) {
-	add_action('admin_notices', function () {
-		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url(admin_url('plugins.php#timber')) . '">' . esc_url(admin_url('plugins.php')) . '</a></p></div>';
-	});
-	return;
-}
+    add_action('admin_notices', function () {
+        echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="'.esc_url(admin_url('plugins.php#timber')).'">'.esc_url(admin_url('plugins.php')).'</a></p></div>';
+    });
 
+    return;
+}
 
 // Create our version of the TimberSite object
 class StarterSite extends TimberSite
 {
+    // This function applies some fundamental WordPress setup, as well as our functions to include custom post types and taxonomies.
+    public function __construct()
+    {
+        add_theme_support('post-formats');
+        add_theme_support('post-thumbnails');
+        add_theme_support('menus');
+        add_filter('timber_context', [$this, 'add_to_context']);
+        add_action('init', [$this, 'register_post_types']);
+        add_action('init', [$this, 'register_taxonomies']);
+        add_action('init', [$this, 'register_menus']);
+        add_action('init', [$this, 'register_widgets']);
+        add_action('init', [$this, 'register_sidebars']);
+        add_action('init', [$this, 'register_shortcodes']);
+        parent::__construct();
+    }
 
-	// This function applies some fundamental WordPress setup, as well as our functions to include custom post types and taxonomies.
-	function __construct()
-	{
-		add_theme_support('post-formats');
-		add_theme_support('post-thumbnails');
-		add_theme_support('menus');
-		add_filter('timber_context', array($this, 'add_to_context'));
-		add_filter('get_twig', array($this, 'add_to_twig'));
-		add_action('init', array($this, 'register_post_types'));
-		add_action('init', array($this, 'register_taxonomies'));
-		add_action('init', array($this, 'register_menus'));
-		add_action('init', array($this, 'register_widgets'));
-		add_action('init', array($this, 'register_sidebars'));
-		add_action('init', array($this, 'register_shortcodes'));
-		parent::__construct();
-	}
+    // Abstracting long chunks of code.
 
+    // The following included files only need to contain the arguments and register_whatever functions. They are applied to WordPress in these functions that are hooked to init above.
 
-	// Abstracting long chunks of code.
+    // The point of having separate files is solely to save space in this file. Think of them as a standard PHP include or require.
 
-	// The following included files only need to contain the arguments and register_whatever functions. They are applied to WordPress in these functions that are hooked to init above.
+    public function register_sidebars()
+    {
+        require 'lib/sidebars.php';
+    }
 
-	// The point of having separate files is solely to save space in this file. Think of them as a standard PHP include or require.
+    public function register_post_types()
+    {
+        require 'lib/custom-types.php';
+    }
 
-	function register_sidebars()
-	{
-		require('lib/sidebars.php');
-	}
+    public function register_taxonomies()
+    {
+        require 'lib/taxonomies.php';
+    }
 
-	function register_post_types()
-	{
-		require('lib/custom-types.php');
-	}
+    public function register_menus()
+    {
+        require 'lib/menus.php';
+    }
 
-	function register_taxonomies()
-	{
-		require('lib/taxonomies.php');
-	}
+    public function register_widgets()
+    {
+        require 'lib/widgets.php';
+    }
 
-	function register_menus()
-	{
-		require('lib/menus.php');
-	}
+    public function register_shortcodes()
+    {
+        require 'lib/shortcodes.php';
+    }
 
-	function register_widgets()
-	{
-		require('lib/widgets.php');
-	}
+    // Access data site-wide.
 
-	function register_shortcodes()
-	{
-		require('lib/shortcodes.php');
-	}
+    // This function adds data to the global context of your site. In less-jargon-y terms, any values in this function are available on any view of your website. Anything that occurs on every page should be added here.
 
-	// Access data site-wide.
+    public function add_to_context($context)
+    {
+        // Our menu occurs on every page, so we add it to the global context.
+        $context['menu'] = new TimberMenu();
+        $context['idea'] = Timber::get_widgets('idea');
+        $context['slider'] = Timber::get_widgets('slider');
+        $context['harmonogram'] = Timber::get_widgets('harmonogram');
+        $context['logistic'] = Timber::get_widgets('logistic');
+        $context['coachs'] = Timber::get_widgets('coachs');
+        $context['singups'] = Timber::get_widgets('singups');
+        $context['contact'] = Timber::get_widgets('contact');
+        $context['newsletter'] = Timber::get_widgets('newsletter');
+        $context['footer1'] = Timber::get_widgets('footer1');
+        $context['footer2'] = Timber::get_widgets('footer2');
+        $context['footer3'] = Timber::get_widgets('footer3');
 
-	// This function adds data to the global context of your site. In less-jargon-y terms, any values in this function are available on any view of your website. Anything that occurs on every page should be added here.
+        // This 'site' context below allows you to access main site information like the site title or description.
+        $context['site'] = $this;
 
-	function add_to_context($context)
-	{
-
-		// Our menu occurs on every page, so we add it to the global context.
-		$context['menu'] = new TimberMenu();
-		$context['idea'] = Timber::get_widgets('idea');
-		$context['slider'] = Timber::get_widgets('slider');
-		$context['harmonogram'] = Timber::get_widgets('harmonogram');
-		$context['logistic'] = Timber::get_widgets('logistic');
-		$context['coachs'] = Timber::get_widgets('coachs');
-		$context['singups'] = Timber::get_widgets('singups');
-		$context['contact'] = Timber::get_widgets('contact');
-		$context['newsletter'] = Timber::get_widgets('newsletter');
-		$context['footer1'] = Timber::get_widgets('footer1');
-		$context['footer2'] = Timber::get_widgets('footer2');
-		$context['footer3'] = Timber::get_widgets('footer3');
-
-		// This 'site' context below allows you to access main site information like the site title or description.
-		$context['site'] = $this;
-		return $context;
-	}
-
-	// Here you can add your own fuctions to Twig. Don't worry about this section if you don't come across a need for it.
-	// See more here: http://twig.sensiolabs.org/doc/advanced.html
-	function add_to_twig($twig)
-	{
-		$twig->addExtension(new Twig_Extension_StringLoader());
-		$twig->addFilter('myfoo', new Twig_Filter_Function('myfoo'));
-		return $twig;
-	}
+        return $context;
+    }
 }
 
 new StarterSite();
-
 
 /*
  *
@@ -115,15 +102,14 @@ new StarterSite();
 // Enqueue scripts
 function my_scripts()
 {
+    // Use jQuery from a CDN
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js', [], null, true);
 
-	// Use jQuery from a CDN
-	wp_deregister_script('jquery');
-	wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js', array(), null, true);
-
-	// Enqueue our stylesheet and JS file with a jQuery dependency.
-	// Note that we aren't using WordPress' default style.css, and instead enqueueing the file of compiled Sass.
-	wp_enqueue_style('studium-nvc-main-styles', get_template_directory_uri() . '/public/main.css', 1.0);
-	wp_enqueue_script('studium-nvc-main-js', get_template_directory_uri() . '/public/main.js', array('jquery'), '1.0.0', true);
+    // Enqueue our stylesheet and JS file with a jQuery dependency.
+    // Note that we aren't using WordPress' default style.css, and instead enqueueing the file of compiled Sass.
+    wp_enqueue_style('studium-nvc-main-styles', get_template_directory_uri().'/public/main.css', 1.0);
+    wp_enqueue_script('studium-nvc-main-js', get_template_directory_uri().'/public/main.js', ['jquery'], '1.0.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'my_scripts');
